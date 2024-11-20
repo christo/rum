57f2d999a215455b894b3fc94ff2539b16465b4a
Chris Mountford <chris.mountford@gmail.com>
2024-11-20 16:54:12 +1100

    add instructions enum

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@ use std::env;
 use std::fs::File;
 use std::io::{self, Read};
 
+#[repr(u8)]
+enum Instructions {
+    ConditionalMove = 0,
+    ArrayIndex,
+    ArrayAmendment,
+    Addition,
+    Multiplication,
+    Division,
+    NotAnd,
+    Halt,
+    Allocation,
+    Abandonment,
+    Output,
+    Input,
+    LoadProgram,
+    Orthography,
+}
+
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
