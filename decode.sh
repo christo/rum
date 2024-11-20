2d12bb53b4a7bb3ce774fda03d884defba671641
Chris Mountford <chris.mountford@gmail.com>
2024-11-20 19:44:02 +1100

    holding it wrong

--- decode.sh
+++ decode.sh
@@ -3,7 +3,7 @@
 echo release build
 cargo build --release
 echo running codex, decrypting
-cat key.txt $(echo p) | target/release/rum codex.umz >output.raw
+cat key.txt | target/release/rum codex.umz >output.raw
 echo stripping prelude of 195 bytes of ascii blurb
 dd if=output.raw of=main.um bs=1 skip=195
 echo main.um stripped image
