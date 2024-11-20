1ee5711698747365393d7d26a6a313c97abd21c9
Chris Mountford <chris.mountford@gmail.com>
2024-11-20 20:48:40 +1100

    tweak

--- decode.sh
+++ decode.sh
@@ -3,7 +3,7 @@
 echo release build
 cargo build --release
 echo running codex, decrypting
-cat key.txt | target/release/rum codex.umz >output.raw
-echo stripping prelude of 195 bytes of ascii blurb
-dd if=output.raw of=main.um bs=1 skip=195
+target/release/rum codex.umz <key.txt >output.raw
+echo stripping prelude of ascii blurb
+dd if=output.raw of=main.um bs=1 skip=198
 echo main.um stripped image
