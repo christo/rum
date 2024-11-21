126627b4ba7711b08d5ffed6de36955a3de340bc
Chris Mountford <chris.mountford@gmail.com>
2024-11-21 12:11:53 +1100

    make even cuter

--- decode.sh
+++ decode.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# post-competition key as provided on boundvariable.org
+DECRYPTION_KEY="(\b.bb)(\v.vv)06FHPVboundvarHRAkz"
+
 echo release build
 cargo build --release
 echo decrypting codex
-(cat key.txt; echo p) | target/release/rum codex.umz | ./strip_prelude.py >main.um
+# p dumps uncompressed, decrypted um image
+(echo "$DECRYPTION_KEY"; echo p) | target/release/rum codex.umz | ./strip_prelude.py >main.um
 echo main.um stripped image
